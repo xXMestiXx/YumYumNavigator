827c633947f44402e7f356aaad156077aa02c52f
Philipp Mestenhauser <philippmestenhauser@Philipps-Air.fritz.box>
2024-06-19 15:05:39 +0200

    merging

--- lib/src/core/constants/strings.dart
+++
@@ -1,6 +0,0 @@
-class Strings {
-  static const onBoardingTitle = "Find the perfect recipe";
-  static const onBoardingSlogan =
-      "More than 20 thousand recipes of healthy and delicious food";
-  static const onBoardingButton = "Get Started";
-}
